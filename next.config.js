ce2ae8e15ed4977b5c8632650ee8d4061fe0adf6
Leandro Oliveira <leandro.gooli@gmail.com>
2025-06-04 15:56:25 +0100

    fix: increase timeout and optimize webpack config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@supabase/supabase-js'],
+  },
+  webpack: (config, { isServer }) => {
+    // Otimizações do webpack
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 };
 
 module.exports = nextConfig; 
